a25d8ffbe1cd81f503c6fb10c4db76cfa3130e60
dcalderonalegre@gmail.com <dcalderonalegre@gmail.com>
2020-09-08 17:28:32 -0500

    Cambiar Css, Agregar JavaScript, Agregar Html

---
+++ script.js
@@ -0,0 +1,17 @@
+function cambiarNombre (idDiv) {
+    let Nombre = prompt( 'Escribe Nuevo Nombre')
+    document.getElementById(idDiv).innerHTML = Nombre
+
+}
+function cambiarNombre2 (idDiv) {
+    let Nombre2 = prompt( 'Escribe Nuevo Nombre')
+    document.getElementById(idDiv).innerHTML = Nombre2
+
+}
+function cambiarNombre3 (idDiv) {
+    let Nombre3 = prompt( 'Escribe Nuevo Nombre')
+    document.getElementById(idDiv).innerHTML = Nombre3
+
+}
+
+
